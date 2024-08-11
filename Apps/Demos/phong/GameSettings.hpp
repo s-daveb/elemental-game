c09bb58acad453745a96a288fd4dded425b60037
S David <2100425+s-daveb@users.noreply.github.com>
2024-08-10 20:33:24 -0400

    Add toml serialization support to GameSettings class

--- Apps/Demos/phong/GameSettings.hpp
+++ Apps/Demos/phong/GameSettings.hpp
@@ -22,6 +22,7 @@ struct GameSettings {
 	RendererSettings renderer_settings;
 
 	JSON_SERIALIZABLE(GameSettings, renderer_settings);
+	TOML_SERIALIZABLE(GameSettings, renderer_settings);
 };
 
 } // namespace elemental
