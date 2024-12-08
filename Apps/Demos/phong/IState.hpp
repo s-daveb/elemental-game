c9a224d22e8e9c357f3e182f61845b6d728fed6a
S David <2100425+s-daveb@users.noreply.github.com>
2024-12-08 01:43:24 -0500

    [WIP] Abstractions for basic games, (states, state stack, etc)

---
+++ Apps/Demos/phong/IState.hpp
@@ -0,0 +1,24 @@
+/*
+ * Copyright © 2024 Saul D. Beniquez
+ * License: Mozilla Public License v. 2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v.2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+#pragma once
+
+namespace elemental {
+
+struct IState {
+	IState() = default;
+	virtual ~IState() = default;
+
+	virtual auto step() -> void = 0;
+};
+
+}
+
+// clang-format off
+// vim: set foldmethod=syntax foldminlines=10 textwidth=80 ts=8 sts=0 sw=8 noexpandtab ft=cpp.doxygen :
