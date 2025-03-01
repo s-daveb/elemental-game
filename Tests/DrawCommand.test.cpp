e996dc48533f283522e616039ec6a5146a9ab0a2
S David <2100425+s-daveb@users.noreply.github.com>
2025-03-01 19:13:18 -0400

    [test-runner] Add unit tests for DrawCmd and IDrawCmd

---
+++ Tests/DrawCommand.test.cpp
@@ -0,0 +1,148 @@
+// clang-format off
+/*
+ * Copyright © 2024 Saul D. Benitez
+ * License: Mozilla Public License v. 2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v.2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+// clang-format on
+
+#include "DrawCommand.hpp"
+#include "IOCore/Exception.hpp"
+
+#include <IOCore/TomlTable.hpp>
+#include <test-utils/common.hpp>
+
+namespace {
+using namespace elemental;
+
+class DummyRenderer : public IRenderer {
+    public:
+	static auto getInstance() -> DummyRenderer&
+	{
+		static DummyRenderer instance;
+		return instance;
+	}
+
+	explicit DummyRenderer() = default;
+
+	void init(RendererSettings& settings) override {}
+
+	auto isInitialized() -> bool override { return true; }
+
+	void deactivate() override {}
+
+	void clearScreen() override {}
+
+	void flip() override {}
+
+	void blit(
+	    std::shared_ptr<void> image_data, const Rectangle& placement
+	) override
+	{
+	}
+
+	[[nodiscard]] auto getResolution() -> Resolution override
+	{
+		return { 0, 0 }; // Default resolution
+	}
+
+	[[nodiscard]] auto getWindowSize() -> Area override
+	{
+		return { 0, 0 }; // Default window size
+	}
+
+	template<typename TR>
+	static auto toRectangle(const TR& data) -> Rectangle
+	{
+		return {}; // Placeholder implementation
+	}
+
+	template<typename TR>
+	static auto fromRectangle(const Rectangle& rectangle) -> TR
+	{
+		return {};
+	}
+};
+} // namespace
+
+BEGIN_TEST_SUITE("elemental::DrawCommand")
+{
+	using namespace elemental;
+
+	TEST_CASE("Basic DrawCommand construction")
+	{
+		Rectangle rect{ { 0, 0 }, { 100, 200 } };
+		std::shared_ptr<void> image_data =
+		    std::make_shared<SDL_Surface>();
+		DummyRenderer renderer;
+
+		DrawCommand cmd(renderer, rect, image_data);
+	}
+
+	TEST_CASE("DrawCommand rectangle getter")
+	{
+		Rectangle rect{ { 0, 0 }, { 100, 200 } };
+		std::shared_ptr<void> image_data = nullptr;
+		DummyRenderer renderer;
+
+		DrawCommand cmd(renderer, rect, image_data);
+
+		// Test rectangle() returns correct value
+		auto result_rect = cmd.rectangle();
+		REQUIRE(result_rect.position.x == rect.position.x);
+		REQUIRE(result_rect.position.y == rect.position.y);
+	}
+
+	TEST_CASE("DrawCommand image data getter")
+	{
+		Rectangle rect{ { 0, 0 }, { 100, 200 } };
+		std::shared_ptr<void> image_data = nullptr;
+		DummyRenderer renderer;
+
+		DrawCommand cmd(renderer, rect, image_data);
+
+		// Test imageData() returns the correct shared_ptr
+		auto result_data = cmd.imageData();
+		REQUIRE(result_data.get() == image_data.get());
+	}
+
+	TEST_CASE("DrawCommand draw method success case")
+	{
+		Rectangle rect{ { 0, 0 }, { 100, 200 } };
+		std::shared_ptr<void> image_data = nullptr;
+		DummyRenderer renderer;
+
+		DrawCommand cmd(renderer, rect, image_data);
+
+		auto result = cmd.draw();
+		REQUIRE(result == kSuccess);
+	}
+
+	TEST_CASE("DrawCommand draw method error case")
+	{
+		Rectangle rect{ { 0, 0 }, { 100, 200 } };
+		std::shared_ptr<void> image_data = nullptr;
+
+		// Override render() to throw an exception
+		class FaultyRenderer : public DummyRenderer {
+		    public:
+			void blit(
+			    std::shared_ptr<void> image_data,
+			    const Rectangle& placement
+			) override
+			{
+				throw std::runtime_error("Rendering failed");
+			}
+		} renderer;
+
+		DrawCommand cmd(renderer, rect, image_data);
+
+		auto result = cmd.draw();
+		REQUIRE(result == kError);
+	}
+}
+// clang-format off
+// vim: set foldmethod=syntax foldminlines=10 textwidth=80 ts=8 sts=0 sw=8 noexpandtab ft=cpp.doxygen :
---
+++ Tests/IDrawCommand.test.cpp
@@ -0,0 +1,62 @@
+/*
+ * Copyright © 2024 Saul D. Beniquez
+ * License: Mozilla Public License v. 2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v.2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+#include "elemental/IDrawCommand.hpp"
+#include "types/rendering.hpp"
+
+#include "test-utils/common.hpp"
+
+#include "IOCore/TomlTable.hpp"
+
+#include <any>
+#include <utility>
+
+namespace {
+using namespace elemental;
+struct DummyDrawCommand : public IDrawCommand {
+	Rectangle rect;
+	std::shared_ptr<void> data;
+
+	DummyDrawCommand(Rectangle& rect, std::shared_ptr<void>& data)
+	    : IDrawCommand(rect, data), rect(rect), data(data)
+	{
+	}
+	~DummyDrawCommand() override {}
+
+	auto rectangle() -> Rectangle& override { return rect; }
+
+	auto imageData() -> std::shared_ptr<void>& override { return data; }
+	auto draw() -> ErrorFlag override { return kSuccess; }
+};
+}
+
+BEGIN_TEST_SUITE("elemental::IDrawCommand")
+{
+	using namespace elemental;
+
+	TEST_CASE("Basic IDrawCommand functionality")
+	{
+		Rectangle rect{ { 0, 0 }, { 100, 200 } };
+		std::shared_ptr<void> image_data = nullptr;
+
+		DummyDrawCommand cmd(rect, image_data);
+
+		// Test getters
+		REQUIRE(cmd.rectangle().position.x == rect.position.x);
+		REQUIRE(cmd.rectangle().position.y == rect.position.y);
+
+		REQUIRE(cmd.imageData() == image_data);
+
+		// Test draw command with valid data
+		cmd.draw();
+	}
+}
+
+// clang-format off
+// vim: set foldmethod=syntax foldminlines=10 textwidth=80 ts=8 sts=0 sw=8 noexpandtab ft=cpp.doxygen :
