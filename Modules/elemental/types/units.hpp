7cef20cb2cf868fbda6364391a260f29b9f5accc
S David <2100425+s-daveb@users.noreply.github.com>
2025-04-06 20:59:26 -0400

    (chore) Fix warning about unit string literals' definition formatting

--- Modules/elemental/types/units.hpp
+++ Modules/elemental/types/units.hpp
@@ -2,21 +2,21 @@
  * Copyright © 2024 Saul D. Beniquez
  * License: Mozilla Public License v. 2.0
  *
- * This Source Code Form is subject to the terms of the Mozilla Public License,
- * v.2.0. If a copy of the MPL was not distributed with this file, You can
- * obtain one at https://mozilla.org/MPL/2.0/.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v.2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
 #pragma once
 
 namespace elemental {
 
-constexpr auto operator"" _Hz(unsigned long long value) -> unsigned long long
+constexpr auto operator""_Hz(unsigned long long value) -> unsigned long long
 {
 	return value;
 }
 
-constexpr auto operator"" _px(unsigned long long value) -> unsigned long long
+constexpr auto operator""_px(unsigned long long value) -> unsigned long long
 {
 	return value;
 }
